ff00e81bf95b5e439d1abf7864b74ed0fafa955c
Asir Shahid <asirshahid@protonmail.com>
2024-05-18 20:26:37 -0400

    Added ags-deps.sh

---
+++ config/scripts/ags-deps.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+# Your code goes here.
+echo 'This is an example shell script'
+echo 'Scripts here will run during build if specified in recipe.yml'
+sudo npm -g install bun
+sudo npm -g install sass
+cargo install cargo-update
+cargo install matugen
+pip3 install materialyoucolor --upgrade
+pip3 install material-color-utilities-python --upgrade
+pip3 install build --upgrade
+pip3 install pillow --upgrade
+pip3 install pywal --upgrade
+pip3 install wheel --upgrade
+pip3 install setuptools-scm --upgrade
